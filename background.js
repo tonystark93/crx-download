0fe6b2d1597ff2843851436949688a0b39395484
Ashokkumar <d.ashokkumar@zoho.com>
2019-03-20 23:51:47 +0530

    changes related to contextmenu.
    Moving all the code to backgroundjs.

--- background.js
+++ background.js
@@ -1,9 +1,75 @@
+let extensionURLPattern = /^https?:\/\/chrome.google.com\/webstore\/.+?\/([a-z]{32})(?=[\/#?]|$)/;
+let currentEXTId = undefined;
+
+function download (downloadAs) {
+    var query = { active: true, currentWindow: true };
+
+    return chrome.tabs.getSelected(null, function (tab) {
+        result = extensionURLPattern.exec(tab.url);
+        if (result[1]) {
+            currentEXTId = result[1];
+            if (downloadAs === "zip") {
+                url = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D' + currentEXTId + '%26installsource%3Dondemand%26uc';
+                downloadZipFile(url, function (blob, publicKey) {
+                    downloadZIP(blob, currentEXTId + ".zip");                   
+                });
+            } else if (downloadAs === "crx") {
+                url = "https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D" + currentEXTId + "%26uc&prodversion=32";
+                console.log(url, currentEXTId)
+                downloadAsCRXFile(url, currentEXTId);
+            }
+        }
+
+    });
+}
+function converToZip (arraybuffer, callback) {
+
+    var data = arraybuffer;
+    var buf = new Uint8Array(data);
+
+    // 43 72 32 34 (Cr24)
+    if (buf[0] !== 67 || buf[1] !== 114 || buf[2] !== 50 || buf[3] !== 52) {
+        throw new Error("Invalid header: Does not start with Cr24.");
+    }
+
+    // 02 00 00 00
+    if (buf[4] !== 2 || buf[5] !== 0 || buf[6] !== 0 || buf[7] !== 0) {
+        throw new Error("Unexpected crx format version number.");
+    }
+
+    var publicKeyLength = 0 + buf[8] + (buf[9] << 8) + (buf[10] << 16) + (buf[11] << 24);
+    var signatureLength = 0 + buf[12] + (buf[13] << 8) + (buf[14] << 16) + (buf[15] << 24);
+
+    // 16 = Magic number (4), CRX format version (4), lengths (2x4)
+    var header = 16;
+    var zipStartOffset = header + publicKeyLength + signatureLength;
+    var zipFragment = new Blob([
+        new Uint8Array(arraybuffer, zipStartOffset)
+    ], {
+            type: 'application/zip'
+        });
+    callback(zipFragment);
+}
+
+function downloadZipFile (url, callback, errCallback, xhrProgressListener) {
+    var requestUrl = url;
+    var x = new XMLHttpRequest();
+    x.open('GET', requestUrl);
+    x.responseType = 'arraybuffer';
+    x.onprogress = xhrProgressListener;
+    x.onload = function () {
+        converToZip(x.response, callback);
+    };
+    x.send();
+}
+
 
 function downloadZIP (blob, fileName) {
     let url=URL.createObjectURL(blob);
     chrome.downloads.download({
         url: url,
-        filename: fileName
+        filename: fileName,
+        saveAs: true
     }, function (downloadId) {
         if (chrome.runtime.lastError) {
             alert('An error occurred while trying to save ' + fileName + ':\n\n' +
@@ -11,10 +77,34 @@ function downloadZIP (blob, fileName) {
         }
     });
 }
-function downloadCRX(url,fileName){
+function downloadAsCRXFile(url,fileName){
     chrome.downloads.download({
         url: url,
         filename: fileName,
         saveAs:true
     });
 }
+chrome.contextMenus.create({
+    'title': 'Download CRX for this extension',
+    'contexts': ['all'],
+   'id':"parent",
+    'documentUrlPatterns': ['https://chrome.google.com/webstore/detail/*']
+});
+chrome.contextMenus.create({
+    'title': 'Download CRX for this extension',
+    'contexts': ['all'],
+    'onclick': function(){
+        download("crx")
+    },
+    parentId: "parent",//No i18n
+    'documentUrlPatterns': ['https://chrome.google.com/webstore/detail/*']
+});
+chrome.contextMenus.create({
+    'title': 'Download ZIP for this extension',
+    'contexts': ['all'],
+    'onclick': function () {
+        download("zip")
+    },
+    parentId: "parent",//No i18n
+    'documentUrlPatterns': ['https://chrome.google.com/webstore/detail/*']
+});
--- popup.js
+++ popup.js
@@ -1,87 +1,34 @@
+
+
 let extensionURLPattern = /^https?:\/\/chrome.google.com\/webstore\/.+?\/([a-z]{32})(?=[\/#?]|$)/;
-let currentEXTId=undefined;
-function download(downloadAs){
-    var query = { active: true, currentWindow: true };
-
-   return chrome.tabs.getSelected(null, function (tab) {
-      result = extensionURLPattern.exec(tab.url);
-         if(result[1]){
-             currentEXTId=result[1];
-             if(downloadAs==="zip"){
-                 downloadZIP(currentEXTId);
-             }else if(downloadAs==="crx"){
-                 var currentEXTId = tab.url.split("/")[6].split('?')[0];
-                
-                 downloadCRX(currentEXTId);
-             }
-         }
-
-    });
-}
 function ready () {
     document.getElementById('downloadZIP').onclick = function(){
-        download("zip");
+        chrome.runtime.getBackgroundPage(function (bg) {
+            bg.download("zip");
+        });
     };
     document.getElementById('downloadCRX').onclick = function () {
-        download("crx");
-    };
-}
-function downloadCRX(extID){
-    url = "https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D" + extID + "%26uc&prodversion=32";
-    console.log(url, extID)
-    chrome.runtime.getBackgroundPage(function (bg) {
-        bg.downloadCRX(url, extID);
-    }); 
-}
-function downloadZIP (extID) {
-    url = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=49.0&x=id%3D'+extID+'%26installsource%3Dondemand%26uc';
-    downloadZipFile(url, function (blob, publicKey) {
         chrome.runtime.getBackgroundPage(function (bg) {
-            bg.downloadZIP(blob, extID+".zip");
-        });       
-    });
-
- }
-
-function converToZip (arraybuffer, callback) {
-    
-        var data = arraybuffer;
-        var buf = new Uint8Array(data);
-
-        // 43 72 32 34 (Cr24)
-        if (buf[0] !== 67 || buf[1] !== 114 || buf[2] !== 50 || buf[3] !== 52) {
-            throw new Error("Invalid header: Does not start with Cr24.");
-        }
-
-        // 02 00 00 00
-        if (buf[4] !== 2 || buf[5] !== 0 || buf[6] !== 0 || buf[7] !== 0) {
-            throw new Error("Unexpected crx format version number.");
-        }
-
-        var publicKeyLength = 0 + buf[8] + (buf[9] << 8) + (buf[10] << 16) + (buf[11] << 24);
-        var signatureLength = 0 + buf[12] + (buf[13] << 8) + (buf[14] << 16) + (buf[15] << 24);
-
-        // 16 = Magic number (4), CRX format version (4), lengths (2x4)
-        var header = 16;
-        var zipStartOffset = header + publicKeyLength + signatureLength;
-       var zipFragment = new Blob([
-            new Uint8Array(arraybuffer, zipStartOffset)
-        ], {
-                type: 'application/zip'
-            });
-        callback(zipFragment);
-    }
-
-function downloadZipFile (url, callback, errCallback, xhrProgressListener) {
-    var requestUrl = url;
-    var x = new XMLHttpRequest();
-    x.open('GET', requestUrl);
-    x.responseType = 'arraybuffer';
-    x.onprogress = xhrProgressListener;
-    x.onload = function () {
-        converToZip(x.response, callback);
+            bg.download("crx");
+        });
     };
-    x.send();
+    chrome.tabs.getSelected(null, function (tab) {
+        var id = extensionURLPattern.exec(tab.url);
+        if(id===null||id[1]===null){
+            document.getElementById("info").style.display="block";
+            var elements = document.getElementsByClassName('buttons');
+            var length = elements.length;
+            for (var i = 0; i < length; i++) {
+                elements[i].style.display = 'none';
+            } 
+        }else{
+            document.getElementById("info").style.display = "none";
+            var elements = document.getElementsByClassName('buttons');
+            var length = elements.length;
+            for (var i = 0; i < length; i++) {
+                elements[i].style.display = 'block';
+            } 
+        }
+    });
 }
-
 ready();
